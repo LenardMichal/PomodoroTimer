edddfbd37de34367d97d90705fd1092604178517
Michał Lenard <lenard.michal@yahoo.com>
2019-11-20 15:29:35 +0100

    preBranching commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,42 @@ import Timer from './components/Timer/Timer';;
 
 
 const App: React.FC = () => {
-  const [workTime, setWorkTime] = useState(0.1 * 60 * 1000);
-  const [shortBrake, setShortBrake] = useState(5 * 60 * 1000);
+  const [workTime, setWorkTime] = useState(0.2 * 60 * 1000);
+  const [shortBrake, setShortBrake] = useState(0.1 * 60 * 1000);
   const [longBrake, setLongBrake] = useState(25 * 60 * 1000);
   const [cycles, setCycles]  = useState(7);
   const [currentCycle, setCurrentCycle] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
-  const [currentCountdown, setCurrentCountdown] = useState(() => workTime);
-  
+  const [currentCountdown, setCurrentCountdown] = useState(0);
+  const [isStarted, setIsStarted] = useState(false);
+
   // Interface to prevent TS error
   interface RefInf {
     current: any
   }
   //Add component variable to handle clears
-  let countdownInterval : RefInf = useRef();
-  // let's useEffect to start countdown
-  useEffect(() => {
-    startTimer();
-    countdownInterval.current = startCountdown(currentCountdown);
-  }, [])
-
+  let countdownTimer : RefInf = useRef();
+  let totalTimer : RefInf = useRef(); 
+ 
   useEffect(() => {
     if(currentCountdown < 1000) {
-      clearInterval(countdownInterval.current);
+      clearInterval(countdownTimer.current);
       navigator.vibrate([900, 300, 900, 300, 900]);
-      rotateCycles();
+      incrementCycle();
       swapTimers();
     }
   }, [currentCountdown]);
   
 function swapTimers() {
-  if(currentCycle % 2 === 0 || currentCycle === 0)  {
-    countdownInterval.current = startCountdown(workTime);
-  } else if( currentCycle !== cycles) {
-    countdownInterval.current = startCountdown(shortBrake);
+  if(currentCycle % 2 === 0)  {
+    countdownTimer.current = startCountdown(workTime);
   } else {
-    countdownInterval.current = startCountdown(longBrake);
+    countdownTimer.current = startCountdown(shortBrake);
   }
 }
 
 
-function rotateCycles() {
+function incrementCycle() {
   setCurrentCycle(prevCycle => {
     if(prevCycle < cycles) {
       return prevCycle + 1;
@@ -74,11 +69,24 @@ function startCountdown(time: number) {
     }, 1000)
 }
 
+function startClocks() {
+  totalTimer.current = startTimer();
+  countdownTimer.current = startCountdown(currentCountdown);
+  setIsStarted(prev => !prev);
+}
+
+function stopClocks() {
+  clearInterval(totalTimer.current);
+  clearInterval(countdownTimer.current);
+  setIsStarted(prev => !prev);
+}
+
   return (
     <div className="App">
       <Timer timer={currentTime}/>
       <Timer timer={currentCountdown} />
-      Cycle: {currentCycle}
+      {currentCycle}
+      {!isStarted ? <button onClick={startClocks}>Start</button> : <button onClick={stopClocks}>Stop</button>}
     </div>
   );
 }
